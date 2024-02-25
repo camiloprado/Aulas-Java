8e57001af5a7fecf1d097237cdcaee86cb43df7f
Camilo~ <camilovgprado21@gmail.comgit>
2024-02-25 16:53:08 -0300

    Lista de Exercicio

---
+++ src/Aluno.java
@@ -0,0 +1,32 @@
+public class Aluno {
+    public int numeroAluno;
+    public String nome;
+    public int idade;
+    public float p1;
+    public float p2;
+
+    public Aluno(int numeroAluno,String nome, int idade, float p1, float p2){
+        this.numeroAluno = numeroAluno;
+        this.nome = nome;
+        this.idade = idade;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public void notaFinal(){
+        System.out.println("A nota final do Aluno é: " + (this.p1 + this.p2)/2);
+    }
+
+    public void dadosAluno(){
+        System.out.println("ID: "+ this.numeroAluno +"\nNome: " + this.nome + "\nIdade: " + this.idade);
+    }
+
+    public void passou(){
+        if ((this.p1 + this.p2)/2 >= 6) {
+            System.out.println("O " + this.nome + " passou de ano.");
+        }
+        else {
+            System.out.println("O " + this.nome + " não passou de ano.");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno obj1 = new Aluno(1, "Camilo", 25, 10, 3 );
+        obj1.notaFinal();
+        obj1.dadosAluno();
+        obj1.passou();
+    }
+}
